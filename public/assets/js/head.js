190665104524b7a4a024affa35f6f9253a7bf7d1
Le Thuan <lengocthuan09@gmail.com>
2026-04-13 08:31:02 +0700

    Update head.js

--- public/assets/js/head.js
+++ public/assets/js/head.js
@@ -1,1 +1,17 @@
-!function(){var u=new URL(location.href);['utm_source','utm_medium','utm_campaign','utm_term','utm_content'].forEach(function(p){u.searchParams.delete(p)});if(u.href!==location.href)history.replaceState(null,'',u)}();
+!function(){
+  var u = new URL(location.href);
+  ['utm_source','utm_medium','utm_campaign','utm_term','utm_content'].forEach(function(p){
+    u.searchParams.delete(p);
+  });
+  if(u.href !== location.href) history.replaceState(null, '', u);
+
+  var ua = navigator.userAgent || '';
+  var isTikTok = /TikTok|musical_ly|BytedanceWebview/i.test(ua);
+  var isInApp = /(FBAN|FBAV|Instagram|Line|Zalo|MiuiBrowser|wv\))/i.test(ua) || /; wv/i.test(ua);
+  var coarse = window.matchMedia && window.matchMedia('(pointer: coarse)').matches;
+  var lite = (isTikTok || isInApp) && coarse;
+
+  if (lite) {
+    document.documentElement.classList.add('perf-lite');
+  }
+}();
